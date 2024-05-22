d1332005fc80c8982d9858c048889148d3c9beb7
Tanguy Barthelemy <tanguy.barthelemy@insee.fr>
2024-05-22 12:20:13 +0000

    version 0.2.4

---
+++ R/QR_export.R
@@ -0,0 +1,251 @@
+#' @title Export des objets QR_matrix dans un fichier Excel
+#'
+#' @description
+#' Permet d'exporter un bilan qualité dans un fichier Excel.
+#'
+#' @param x objet de type \code{\link{QR_matrix}}.
+#' @param layout composantes du bilan à exporter. Par défaut, \code{layout = "all"} : la matrice des modalités
+#' (\code{"modalities"}) et celle des valeurs (\code{"values"}) sont exportées. Pour exporter la matrice
+#' des modalités avec en plus les variables supplémentaires de la matrice des valeurs, utiliser
+#' \code{layout = "combined"}.
+#' @param create booléen indiquant s'il faut créer le fichier Excel ou non (\code{create = TRUE} par défaut)
+#' @param clear_sheet booléen indiquant s'il faut nettoyer les feuilles du fichier Excel avant l'export (
+#' \code{clear_sheet = TRUE} par défaut).
+#' @param auto_format booléen indiquant s'il faut formatter la sortie (\code{auto_format = TRUE} par défaut).
+#' @param file_name argument optionnel permettant de choisir le chemin et le nom du fichier à exporter. S'il n'est pas spécifié,
+#' un fichier *export.xls* sera créé dans le working directory.
+#' @param sheet_names nom des feuilles Excel en sortie. S'il n'est pas spécifié, le nom sera celui de la composante exportée.
+#' Si le paramètre est spécifié, les éventuelles feuilles contenant ces noms sont supprimées.
+#' @param ... autres paramètres non utilisés.
+#'
+#' @returns Renvoie de manière invisible (via \code{invisible(x)}) un objet de classeur créé par \code{XLConnect::loadWorkbook()} pour une manipulation ultérieure.
+#'
+#' @keywords internal
+#' @name fr-export_xlsx.QR_matrix
+NULL
+#> NULL
+
+
+#' @title Exporting QR_matrix objects in an Excel file
+#'
+#' @description
+#' To export a quality report in an Excel file.
+#'
+#' @param x a \code{\link{QR_matrix}} object.
+#' @param layout the components of the report to export. By default, \code{layout = "all"}: the matrices modalities
+#' (\code{"modalities"}) and values (\code{"values"}) are exported in separate files. To export them in a single file (in two sheets),
+#' use \code{layout = "combined"}.
+#' @param create logical indicating whether to create an Excel file if it doesn't exist yet (\code{create = TRUE} by default)
+#' @param clear_sheet logical indicating whether to clear the Excel sheets before the export (\code{clear_sheet = TRUE} by default).
+#' @param auto_format logical indicating whether to format the output (\code{auto_format = TRUE} by default).
+#' @param file_name optional argument to choose the path and name of the file to export. If not specified, an *export.xls* will be created in the working directory.
+#' @param sheet_names names of the exported Excel sheets. If not specified, the sheets will be named after the exported components.
+#' If specified, existing sheets with these names will be overwritten.
+#' @param ... other unused parameters.
+#'
+#' @returns Returns invisibly (via \code{invisible(x)}) a workbook object created by \code{XLConnect::loadWorkbook()} for further manipulation.
+#'
+#' @family QR_matrix functions
+#' @seealso [Traduction française][fr-export_xlsx.QR_matrix()]
+#' @export
+export_xlsx.QR_matrix <- function(x, layout = c("all", "modalities", "values", "combined"),
+                                  create = TRUE, clear_sheet = TRUE, auto_format = TRUE,
+                                  file_name, sheet_names, ...) {
+    layout <- match.arg(layout)
+    file_name <- ifelse(missing(file_name), "export.xls", file_name)
+    wb <- XLConnect::loadWorkbook(filename = file_name, create = create)
+    sheets <- switch(layout,
+        all = c("modalities", "values"),
+        combined = "values",
+        layout
+    )
+    exp_data <- switch(layout,
+        combined = {
+            data_v <- x[["values"]]
+            data_m <- x[["modalities"]]
+            joint_names <- colnames(data_m)[colnames(data_m) %in% colnames(data_v)]
+            data_v[, joint_names] <- data_m[, joint_names]
+            list(values = data_v)
+        },
+        x
+    )
+    XLConnect::createSheet(wb, sheets)
+    if (clear_sheet) {
+        XLConnect::clearSheet(wb, sheets)
+    }
+
+    XLConnect::setStyleAction(wb, XLConnect::XLC$STYLE_ACTION.DATA_FORMAT_ONLY)
+    if (auto_format) {
+        XLConnect::setDataFormatForType(wb,
+            type = XLConnect::XLC$DATA_TYPE.NUMERIC,
+            format = "0.000"
+        )
+
+        cs <- XLConnect::createCellStyle(wb)
+        XLConnect::setBorder(cs,
+            side = "all",
+            type = XLConnect::XLC$BORDER.THIN,
+            color = XLConnect::XLC$COLOR.BLACK
+        )
+    }
+    for (s in sheets) {
+        data <- exp_data[[s]]
+        XLConnect::writeWorksheet(wb, data = data, sheet = s, header = TRUE)
+        if (auto_format) {
+            XLConnect::setCellStyle(wb,
+                sheet = s, row = 1:(nrow(data) + 1),
+                col = seq_len(ncol(data)), cellstyle = cs
+            )
+            XLConnect::setCellStyle(wb,
+                formula = paste0(
+                    s, "!", "$A$1:",
+                    XLConnect::idx2cref(c(nrow(data) + 1, ncol(data)))
+                ),
+                cellstyle = cs
+            )
+            XLConnect::setColumnWidth(wb,
+                sheet = s, column = seq_len(ncol(data)),
+                width = -1
+            )
+        }
+    }
+    if (!missing(sheet_names) && length(sheet_names) == length(sheets)) {
+        XLConnect::removeSheet(wb, sheet = sheet_names)
+        XLConnect::renameSheet(wb, sheets, sheet_names)
+    }
+    XLConnect::saveWorkbook(wb)
+    return(invisible(wb))
+}
+
+#' @title Exporting QR_matrix or mQR_matrix objects in an Excel file
+#'
+#' @param x a \code{\link{QR_matrix}} or \code{\link{mQR_matrix}} object.
+#' @param ... other parameters of the function \code{\link{export_xlsx.QR_matrix}}.
+#'
+#' @returns
+#' If \code{x} is a \code{\link{mQR_matrix}}, the function returns invisibly (via \code{invisible(x)}) the same \code{\link{mQR_matrix}} object as \code{x}.
+#' Else if \code{x} is a \code{\link{QR_matrix}}, the function returns invisibly (via \code{invisible(x)}) a workbook object created by \code{XLConnect::loadWorkbook()} for further manipulation.
+#'
+#' @family QR_matrix functions
+#' @export
+export_xlsx <- function(x, ...) {
+    UseMethod("export_xlsx", x)
+}
+
+#' @family QR_matrix functions
+#' @export
+export_xlsx.default <- function(x, ...) {
+    stop("A QR_matrix or mQR_matrix object is required!")
+}
+
+
+
+
+#' Export des objets mQR_matrix dans des fichiers Excel
+#'
+#' Permet d'exporter dans des fichiers Excel une liste de bilan qualité
+#'
+#' @param x objet de type \code{\link{mQR_matrix}} à exporter.
+#' @param export_dir dossier d'export des résultats.
+#' @param layout_file paramètre d'export. Par défaut, (\code{layout_file = "ByComponent"}) et un fichier Excel est exporté par composante
+#' de la matrice bilan qualité (matrice des modalités ou des valeurs), dont chaque feuille correspond à un bilan qualité. Pour avoir
+#' un fichier par bilan qualité dont chaque feuille correspond à la composante exportée, utiliser \code{layout_file = "ByQRMatrix"}.
+#' @param file_extension extension des fichiers (\code{".xls"} ou \code{".xlsx"}).
+#' @param layout composantes du bilan à exporter : voir \code{\link{export_xlsx.QR_matrix}} .
+#' @param ... autres paramètres de la fonction \code{\link{export_xlsx.QR_matrix}}.
+#'
+#' @returns Renvoie de manière invisible (via \code{invisible(x)}) le même objet \code{\link{mQR_matrix}} que \code{x}.
+#'
+#' @keywords internal
+#' @name fr-export_xlsx.mQR_matrix
+NULL
+#> NULL
+
+
+#' Exporting mQR_matrix objects in Excel files
+#'
+#' To export several quality reports in Excel files
+#'
+#' @param x a \code{\link{mQR_matrix}} object to export.
+#' @param export_dir export directory.
+#' @param layout_file export parameter. By default, (\code{layout_file = "ByComponent"}) and an Excel file is exported for each part of the
+#' quality report matrix (modalities and values matrices). To group both modalities and values reports/sheets into a single Excel file, use the option \code{layout_file = "ByQRMatrix"}.
+#' @param file_extension possible values are \code{".xls"} and \code{".xlsx"}.
+#' @param layout elements of the report to export: see \code{\link{export_xlsx.QR_matrix}} .
+#' @param ... other parameters of the function \code{\link{export_xlsx.QR_matrix}}.
+#'
+#' @returns Returns invisibly (via \code{invisible(x)}) the same \code{\link{mQR_matrix}} object as \code{x}.
+#'
+#' @family QR_matrix functions
+#' @seealso [Traduction française][fr-export_xlsx.mQR_matrix()]
+#' @export
+export_xlsx.mQR_matrix <- function(x, export_dir = "./",
+                                   layout_file = c("ByComponent", "ByQRMatrix"),
+                                   file_extension = c(".xls", ".xlsx"),
+                                   layout = c("all", "modalities", "values", "combined"),
+                                   ...) {
+    if (length(x) == 0) {
+        return(invisible(x))
+    }
+    file_extension <- match.arg(file_extension)
+    layout_file <- match.arg(layout_file)
+    layout <- match.arg(layout)
+
+    QR_matrix_names <- names(x)
+
+    if (is.null(QR_matrix_names)) {
+        QR_matrix_names <- paste0("QR_", seq_along(x))
+    } else {
+        QR_matrix_names[is.na(QR_matrix_names)] <- ""
+        if (!is.na(match("", QR_matrix_names))) {
+            QR_matrix_names[match("", QR_matrix_names)] <- paste0(
+                "QR_",
+                match("", QR_matrix_names)
+            )
+        }
+    }
+
+
+    if (layout_file == "ByQRMatrix") {
+        # To export a quality report per file:
+        files_name <- normalizePath(
+            file.path(
+                export_dir,
+                paste0(QR_matrix_names, file_extension)
+            ),
+            mustWork = FALSE
+        )
+        for (i in seq_along(x)) {
+            export_xlsx(x[[i]], layout = layout, file_name = files_name[i], ...)
+        }
+    } else {
+        # To export a file per element of the quality report
+        files_name <- switch(layout,
+            all = c("modalities", "values"),
+            combined = "values",
+            layout
+        )
+        final_layout <- switch(layout,
+            all = c("modalities", "values"),
+            layout
+        )
+
+        files <- normalizePath(
+            path = file.path(export_dir, paste0(files_name, file_extension)),
+            mustWork = FALSE
+        )
+        for (i in seq_along(x)) {
+            # Index on the QR_matrix
+            for (j in seq_along(final_layout)) {
+                # Index on the elements
+                export_xlsx(x[[i]],
+                    layout = final_layout[j], file_name = files[j],
+                    sheet_names = QR_matrix_names[i],
+                    ...
+                )
+            }
+        }
+    }
+
+    return(invisible(x))
+}
---
+++ R/QR_matrix_classes.R
@@ -0,0 +1,327 @@
+#' Objets bilan qualité
+#'
+#' \code{QR_matrix()} permet de créer un objet de type \code{\link{QR_matrix}} contenant un bilan qualité.
+#'
+#' \code{mQR_matrix()} permet de créer un objet de type \code{\link{mQR_matrix}} qui est une liste de bilans qualité (donc d'objets \code{\link{QR_matrix}}).
+#'
+#' \code{is.QR_matrix()} et \code{is.mQR_matrix()} permettent de tester si un objet est un bilan qualité ou une liste de bilans qualité.
+#'
+#'
+#' @param modalities un \code{data.frame} contenant les modalités (Good, Bad, etc.)
+#' associées aux variables.
+#' @param values un \code{data.frame} contenant les valeurs (p-valeurs des tests, statistiques, etc.)
+#' associées aux variables. Peut donc contenir plus de variables que le data.frame \code{modalities}.
+#' @param score_formula formule utilisée pour calculer le score global (s'il existe).
+#' @param x un objet de type \code{\link{QR_matrix}}, \code{\link{mQR_matrix}} ou une liste d'objets \code{\link{QR_matrix}}.
+#' @param ... des objets du même type que \code{x}.
+#' @details Un objet  de type \code{\link{QR_matrix}} est une liste de trois paramètres :
+#' * le paramètre \code{modalities} est un \code{data.frame} contenant un ensemble de variables sous forme catégorielle
+#'   (par défaut : Good, Uncertain, Bad, Severe).
+#' * le paramètre \code{values}  est un \code{data.frame} contenant les valeurs associées aux indicateurs présents dans
+#'   \code{modalities} (i.e. : p-valeurs, statistiques, etc.), ainsi que des variables qui n'ont pas
+#'   de modalité (i.e. : fréquence de la série, modèle ARIMA, etc).
+#' * le paramètre \code{score_formula} contient la formule utilisée pour calculer le score (une fois le calcul réalisé).
+#'
+#' @returns
+#' \code{QR_matrix()} crée et renvoie un objet \code{\link{QR_matrix}}.
+#' \code{mQR_matrix()} crée et renvoie un objet \code{\link{mQR_matrix}} (c'est-à-dire une liste d'objets \code{\link{QR_matrix}}).
+#' \code{is.QR_matrix()} et \code{is.mQR_matrix()} renvoient des valeurs booléennes (\code{TRUE} ou \code{FALSE}).
+#'
+#' @encoding UTF-8
+#' @keywords internal
+#' @name fr-QR_matrix
+NULL
+#> NULL
+
+
+#' @title Quality report objects
+#'
+#' @description
+#' \code{mQR_matrix()} and \code{QR_matrix()} are creating one (or several) quality report. The function
+#' \code{is.QR_matrix()} and \code{is.mQR_matrix()} are functions to test whether an object is a quality report or a list of quality reports.
+#'
+#' @param modalities a \code{data.frame} containing the output variables' modalities (Good, Bad, etc.)
+#' @param values a \code{data.frame} containing the output variables' values (test p-values, test statistics, etc.) Therefore, the values data frame can contain more variables
+#' than the data frame \code{modalities}.
+#' @param score_formula the formula used to calculate the series score (if defined).
+#' @param x a \code{\link{QR_matrix}} object, a \code{\link{mQR_matrix}} object or a list of \code{\link{QR_matrix}} objects.
+#' @param ... objects of the same type as \code{x}.
+#'
+#' @details A\code{\link{QR_matrix}} object is a list of three items:
+#' * \code{modalities}, a \code{data.frame} containing a set of categorical variables (by default: Good, Uncertain, Bad, Severe).
+#' * \code{values}, a \code{data.frame} containing the values corresponding to the \code{modalities} indicators (i.e. p-values, statistics, etc.),
+#' as well as variables for which a modality cannot be defined (e.g. the series frequency, the ARIMA model, etc).
+#' * \code{score_formula} contains the formula used to calculate the series score (once the calculus is done).
+#'
+#' @returns
+#' \code{QR_matrix()} creates and returns a \code{\link{QR_matrix}} object.
+#' \code{mQR_matrix()} creates and returns a \code{\link{mQR_matrix}} object (ie. a list of \code{\link{QR_matrix}} objects).
+#' \code{is.QR_matrix()} and \code{is.mQR_matrix()} return Boolean values (\code{TRUE} or \code{FALSE}).
+#'
+#' @encoding UTF-8
+#' @name QR_matrix
+#' @seealso [Traduction française][fr-QR_matrix()]
+#' @export
+QR_matrix <- function(modalities = NULL, values = NULL, score_formula = NULL) {
+    QR <- list(
+        modalities = modalities, values = values,
+        score_formula = score_formula
+    )
+    class(QR) <- c("QR_matrix")
+    QR
+}
+#' @export
+#' @rdname QR_matrix
+mQR_matrix <- function(x = list(), ...) {
+    UseMethod("mQR_matrix", x)
+}
+#' @rdname QR_matrix
+#' @export
+is.QR_matrix <- function(x) {
+    return(inherits(x, "QR_matrix"))
+}
+#' @export
+mQR_matrix.QR_matrix <- function(x = QR_matrix(), ...) {
+    mQR <- c(list(x), list(...))
+    class(mQR) <- "mQR_matrix"
+    return(mQR)
+}
+#' @export
+mQR_matrix.default <- function(x = list(), ...) {
+    mQR <- c(x, list(...))
+    class(mQR) <- "mQR_matrix"
+    return(mQR)
+}
+#' @export
+mQR_matrix.mQR_matrix <- function(x = mQR_matrix.default(), ...) {
+    mQR <- c(x, ...)
+    class(mQR) <- "mQR_matrix"
+    return(mQR)
+}
+#' @rdname QR_matrix
+#' @export
+is.mQR_matrix <- function(x) {
+    return(inherits(x, "mQR_matrix"))
+}
+
+
+
+#' Affichage des objets QR_matrix et mQR_matrix
+#'
+#' Pour afficher un objet QR_matrix ou mQR_matrix.
+#'
+#' @param x objet de type \code{\link{mQR_matrix}} ou \code{\link{mQR_matrix}}.
+#' @param print_variables booléen pour imprimer ou non les noms des indicateurs (supplémentaire inclus).
+#' @param print_score_formula booléen pour imprimer ou non la formule qui a servi à calculer le score (le cas échéant).
+#' @param score_statistics booléen pour imprimer ou non des statistiques sur les scores de la \code{\link{mQR_matrix}} (le cas échéant).
+#' @param ... autres arguments non utilisés.
+#' @returns la méthode \code{print} imprime un objet \code{\link{mQR_matrix}} ou \code{\link{mQR_matrix}} et le renvoie de manière invisible (via \code{invisible(x)}).
+#' @encoding UTF-8
+#' @keywords internal
+#' @name fr-print.QR_matrix
+NULL
+#> NULL
+
+
+#' @title Printing QR_matrix and mQR_matrix objects
+#'
+#' @description
+#' To print information on a QR_matrix or mQR_matrix object.
+#'
+#' @param x a \code{\link{mQR_matrix}} or \code{\link{mQR_matrix}} object.
+#' @param print_variables logical indicating whether to print the indicators' name (including additionnal variables).
+#' @param print_score_formula logical indicating whether to print the formula with which the score was calculated (when calculated).
+#' @param score_statistics logical indicating whether to print the statistics in the \code{\link{mQR_matrix}} scores (when calculated).
+#' @param ... other unused arguments.
+#'
+#' @returns the \code{print} method prints a \code{\link{mQR_matrix}} or \code{\link{mQR_matrix}} object and returns it invisibly (via \code{invisible(x)}).
+#'
+#' @encoding UTF-8
+#' @name print.QR_matrix
+#' @seealso [Traduction française][fr-print.QR_matrix()]
+#' @export
+print.QR_matrix <- function(x, print_variables = TRUE, print_score_formula = TRUE, ...) {
+    nb_var <- nrow(x$modalities)
+    nb_var_modalities <- ncol(x$modalities)
+    nb_var_values <- ncol(x$values)
+
+    if (is.null(nb_var)
+        || is.null(nb_var_modalities)
+        || is.null(nb_var_values)
+        || nb_var * nb_var_modalities * nb_var_values == 0) {
+        cat("The quality report matrix is empty")
+        return(invisible(x))
+    }
+    cat(sprintf(
+        ngettext(
+            nb_var, "The quality report matrix has %d observations",
+            "The quality report matrix has %d observations"
+        ),
+        nb_var
+    ))
+    cat("\n")
+    cat(sprintf(
+        ngettext(
+            nb_var_modalities, "There are %d indicators in the modalities matrix",
+            "There are %d indicators in the modalities matrix"
+        ),
+        nb_var_modalities
+    ))
+    cat(sprintf(
+        ngettext(
+            nb_var_values, " and %d indicators in the values matrix",
+            " and %d indicators in the values matrix"
+        ),
+        nb_var_values
+    ))
+    cat("\n")
+    if (print_variables) {
+        cat("\n")
+        names_var_modalities <- colnames(x$modalities)
+        names_var_values <- colnames(x$values)
+        names_var_values_sup <- names_var_values[!names_var_values %in% names_var_modalities]
+        names_var_modalities <- paste(names_var_values, collapse = "  ")
+        names_var_values_sup <- paste(names_var_values_sup, collapse = "  ")
+
+
+        cat(sprintf(
+            "The quality report matrix contains the following variables:\n%s\n",
+            names_var_modalities
+        ))
+        cat("\n")
+        if (all(names_var_values_sup == "")) {
+            cat("There's no additionnal variable in the values matrix")
+        } else {
+            cat(sprintf(
+                "The variables exclusively found in the values matrix are:\n%s",
+                names_var_values_sup
+            ))
+        }
+        cat("\n")
+        if (length(names_var_values_sup) > 1) {
+            cat(sprintf(
+                ngettext(
+                    length(names_var_values_sup),
+                    "There's no additionnal variable in the values matrix",
+                    "The variables exclusively found in the values matrix are:\n%s"
+                ),
+                names_var_values_sup
+            ))
+        }
+
+        cat("\n")
+    }
+
+
+    score_value <- extract_score(x, format_output = "vector")
+    if (is.null(score_value)) {
+        cat("No score was calculated")
+    } else {
+        cat(sprintf(
+            "The smallest score is %1g and the greatest is %2g\n",
+            min(score_value, na.rm = TRUE), max(score_value, na.rm = TRUE)
+        ))
+        cat(sprintf(
+            "The average score is %1g and its standard deviation is %2g",
+            mean(score_value, na.rm = TRUE), sd(score_value, na.rm = TRUE)
+        ))
+    }
+    if (print_score_formula && !is.null(x$score_formula)) {
+        cat("\n\n")
+        cat(sprintf(
+            "The following formula was used to calculate the score:\n%s",
+            as.character(x$score_formula)
+        ))
+    }
+    return(invisible(x))
+}
+
+#' @rdname print.QR_matrix
+#' @export
+print.mQR_matrix <- function(x, score_statistics = TRUE, ...) {
+    if (length(x) == 0) {
+        cat("List without a quality report")
+        return(invisible(x))
+    }
+    cat(sprintf(
+        ngettext(
+            length(x), "The object contains %d quality report(s)",
+            "The object contains %d quality report(s)"
+        ),
+        length(x)
+    ))
+    cat("\n")
+    bq_names <- names(x)
+    bq_names[is.na(bq_names)] <- ""
+    if (is.null(bq_names) || all(is.na(bq_names))) {
+        cat("No quality report is named")
+    } else {
+        bq_names_na <- sum(is.na(bq_names))
+        bq_valid_names <- bq_names[!is.na(bq_names)]
+        cat(sprintf(
+            ngettext(
+                length(bq_valid_names),
+                "%d quality report is named: %s",
+                "%d quality reports are named: %s"
+            ),
+            length(bq_valid_names), paste(bq_valid_names, collapse = "  ")
+        ))
+
+        if (length(bq_names_na) > 1) {
+            cat("\n")
+            cat(sprintf(
+                ngettext(
+                    bq_names_na,
+                    "%d quality report isn't named",
+                    "%d quality reports aren't named"
+                ),
+                bq_names_na
+            ))
+        }
+    }
+    if (score_statistics) {
+        cat("\n")
+        score_values <- extract_score(x, format_output = "vector")
+        all_score <- do.call(c, score_values)
+        if (is.null(all_score)) {
+            cat("No quality report has a calculated score")
+        } else {
+            cat(sprintf(
+                "The average score over all quality reports is %g\n",
+                mean(all_score, na.rm = TRUE)
+            ))
+            cat(sprintf(
+                "The smallest score is %1g and the greatest is %2g\n",
+                min(all_score, na.rm = TRUE), max(all_score, na.rm = TRUE)
+            ))
+
+            for (i in seq_along(score_values)) {
+                cat("\n\n")
+                score_value <- score_values[[i]]
+
+                bq_name <- bq_names[i]
+                if (is.null(bq_name) || is.na(bq_name)) {
+                    bq_name <- ""
+                } else {
+                    bq_name <- paste0(" (", bq_name, ")")
+                }
+
+                if (is.null(score_value)) {
+                    cat(sprintf("There is no calculated score for the quality report n.%d%s", i, bq_name))
+                } else {
+                    cat(sprintf(
+                        "The quality report n.%d%s has an average score of %g\n", i, bq_name,
+                        mean(score_value, na.rm = TRUE)
+                    ))
+                    cat(sprintf(
+                        "The smallest score is %1g and the greatest is %2g\n",
+                        min(score_value, na.rm = TRUE), max(score_value, na.rm = TRUE)
+                    ))
+                }
+            }
+        }
+    }
+
+
+    return(invisible(x))
+}
